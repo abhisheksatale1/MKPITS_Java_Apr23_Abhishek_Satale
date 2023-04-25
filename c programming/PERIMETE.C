63d51d1b3acfb051ee7b1a13a9f84e05d7c73d05
Abhishek Satale <abhisheksatale4@gmail.com>
2023-04-25 17:51:48 +0530

    17.04.2023

---
+++ c programming/PERIMETE.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main (){
+float num1, num2, per;
+printf("enter the value");
+scanf("%f%f", &num1, &num2);
+per=num1*2+num2*2;
+printf("%f", per);
+getch();
+}
