fae4c930bd11cb4ba81dd7b6662837e295280f1e
Abhishek Satale <abhisheksatale4@gmail.com>
2023-04-25 17:11:43 +0530

    17.04.2023

---
+++ c programming/DIVISION.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main (){
+clrscr();
+int num1, num2, res;
+printf("enter the value");
+scanf("%d%d", &num1, &num2);
+res= num1 / num2;
+printf("%d",res);
+getch();
+}
---
+++ c programming/XLM.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main (){
+clrscr();
+char char1 = 'x';
+char char2 = 'M';
+char char3 = 'L';
+printf("The reverse of %c%c%c is %c%c%c\n",
+	char1, char2, char3,
+	char3, char2, char1);
+	getch();
+
+	}
