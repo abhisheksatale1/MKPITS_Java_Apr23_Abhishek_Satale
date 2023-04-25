9da59d186d7becc6d4895b62bcc82da532295e37
Abhishek Satale <abhisheksatale4@gmail.com>
2023-04-25 14:52:42 +0530

    17.04.2024

---
+++ c programming/24-4-23.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int row,col;
+clrscr();
+    for(row=1;row<=3;row++)
+  {
+	  for(col=1;col<=5;col++)
+	 {
+	     printf("*");
+	 }
+	 printf("\n");
+
+    }
+    getch();
+    }
---
+++ c programming/NAME.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+printf("Name : Abhishek satale \n Dob: jan25, 2000\n Mobile : 7499405548");
+getch();
+};
+
