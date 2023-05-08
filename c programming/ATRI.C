ffb7cbe381fd9ea0eae5befa5c3439928f576455
Abhishek Satale <abhisheksatale4@gmail.com>
2023-05-08 17:34:03 +0530

    Area of Triangle

---
+++ c programming/ATRI.C
@@ -0,0 +1,13 @@
+//area of triangle
+#include<stdio.h>
+#include<conio.h>
+void funt(){
+float l,b,res;
+scanf("%f%f", &l,&b);
+res=0.5*l*b;
+printf("%f",res);
+}
+void main (){
+funt();
+getch();
+}
