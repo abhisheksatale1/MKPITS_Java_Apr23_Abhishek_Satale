a61fe0bbef0cba7bfa954ff06f6f4f10d5c4df4a
Abhishek Satale <abhisheksatale4@gmail.com>
2023-05-08 17:26:17 +0530

    Area of circle

---
+++ c programming/AOFCIRCL.C
@@ -0,0 +1,13 @@
+//no argument with no return type
+#include<stdio.h>
+#include<conio.h>
+void fun(){
+   int pi=3.14,r,res;
+   scrclr();
+   scanf("%d",&r);
+   res=pi*r*r;
+   printf("%d",res);
+   }
+   void main(){
+   fun();
+   }
