c0ad25038b262f74ddbc66a0991da48c34c467ce
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-06-22 14:02:45 +0530

    pratice files of java and javascript

---
+++ Java/Wrapperclass/WrapperCharacter.java
@@ -0,0 +1,9 @@
+class WrapperCharacter{
+public static void main(String []args){
+ char firstname= 'a';
+ Character secondname= Character.valueOf(firstname);
+ Character thirdname=firstname;
+ System.out.println(firstname+" "+secondname+" "+thirdname);
+}
+}
+ 
