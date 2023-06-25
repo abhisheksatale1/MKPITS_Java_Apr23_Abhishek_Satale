f0dd4c747f02439f48284c2541fe0b9c8b4b2f9a
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-06-25 14:58:45 +0530

    lOop

---
+++ Java/Loop/Conversion.java
@@ -0,0 +1,18 @@
+class Conversion{
+	public void conve( String call){
+		switch(call){
+			case "hi":
+			System.out.println("hello");
+			break;
+			case "his":
+			System.out.println("her");
+			break;
+			case"by":
+			System.out.println("by by");
+			break;
+			default:
+			System.out.println("I am busy");
+		}
+	}
+}
+			
---
+++ Java/Loop/Loop.java
@@ -0,0 +1,8 @@
+class Loop{
+public static void main (String []args){
+	int i =0;
+	for(i=0; i<100; i++){
+	System.out.println(i);
+	}
+	}
+}
