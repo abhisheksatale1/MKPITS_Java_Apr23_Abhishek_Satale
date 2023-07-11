a482e06bf16ac4c0d862a3440c93b323f35cba0b
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-07-11 12:03:26 +0530

    string

---
+++ Java/String/StringCheck.java
@@ -0,0 +1,10 @@
+class StringCheck{
+	public static void main(String[]args){
+	String str1="abc";
+	String str2="abc";
+	int hashCode(int abc);
+	System.out.println(str1==str2);
+	System.out.println(str1.equals(str2));
+	System.out.println(hasCode);
+	}
+}
