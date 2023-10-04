26586670962aac144c64ddc60b2f03ad8fc83fd3
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-10-04 20:56:19 +0530

    QueryPratice

---
+++ Database_SQL/CreatingQuery.sql
@@ -0,0 +1,14 @@
+create table users
+(
+id int unsigned,
+name varchar(100),
+email varchar(150),
+password varchar(100),
+contact varchar(15),
+address text,
+dob date,
+gender enum("M","F","O"),
+status boolean
+)
+show tables
+select * from users
---
+++ Database_SQL/QueryFirst.sql
@@ -0,0 +1,3 @@
+use myfirstdatabase
+INSERT INTO USERS values(111,'abhishek',"abhishek@gmail.com",12345,182848282,'nanded','2000-01-25',"m",1)	
+select * from users
