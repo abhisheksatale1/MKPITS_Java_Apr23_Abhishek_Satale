62c37bdfc733f1ea7f90fc055b945f4b74f39749
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-10-25 23:15:41 +0530

    servlet connection with database

---
+++ Java/Servlet/MyFirstServlet/connection.java
@@ -0,0 +1,20 @@
+
+import java.sql.PreparedStatement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abhishek
+ */
+class connection {
+
+    static PreparedStatement prepareStatement(String insert_into_values) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
