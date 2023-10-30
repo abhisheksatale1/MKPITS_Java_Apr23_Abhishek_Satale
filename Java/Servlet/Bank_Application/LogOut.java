7e7ba4ce47bc332d904ea4b1fb379a64c9891219
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-10-31 00:48:21 +0530

    Bank application

---
+++ Java/Servlet/Bank_Application/LogOut.java
@@ -0,0 +1,36 @@
+package Bank_Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogOut extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession httpSession = request.getSession();
+            httpSession.invalidate();
+
+            // Redirect to the logout.html page
+            response.sendRedirect("logout.html");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
--- Java/Servlet/Bank_Application/Statement.java
+++ Java/Servlet/Bank_Application/Statement.java
@@ -1,11 +1,5 @@
 package Bank_Servlets;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -13,133 +7,109 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Abhishek
- */
 public class Statement extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     * @throws java.sql.SQLException
-     * @throws java.lang.ClassNotFoundException
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            
-            
 
             Class.forName("com.mysql.cj.jdbc.Driver");
-            out.println("driver loaded");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "Abhi@123");
-                out.println("connection established");
-                
-                HttpSession httpsession = request.getSession(true);
+
+            HttpSession httpsession = request.getSession(true);
             String Userid = httpsession.getAttribute("u_id").toString();
-            out.println("<h1>UserId : "+Userid+"</h1>");
-            
+
             PreparedStatement preparedstatement = connection.prepareStatement("select Transaction_date, Amount, Amount_type from Transactions where user_id=?");
-                    preparedstatement.setString(1, Userid);
-                    ResultSet resultSet = preparedstatement.executeQuery(); 
-            /* TODO output your page here. You may use following sample code. */
-            
-                  
+            preparedstatement.setString(1, Userid);
+            ResultSet resultSet = preparedstatement.executeQuery();
+
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Statement</title>");            
+            out.println("<title>SBI Statement</title>");
+            out.println("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>"); // Add Bootstrap CSS
             out.println("</head>");
             out.println("<body>");
-            out.println("<table align=center border=1 cellspacing=3 cellpadding=10>"
-            + "<tr>"
-                    +"<th>Transaction Date</th>"
-                    +"<th>Amount</th>"
-                    +"<th>Transaction Type</th>"
-            +"</tr>");
-             
-            while(resultSet.next()){
-            out.println("<tr>"
-                    +"<td>"+resultSet.getDate(1)+"</td>"    
-                    +"<td>"+resultSet.getDouble(2)+"</td>"    
-                    +"<td>"+resultSet.getString(3)+"</td>"    
-                    + "</tr>");
-              }
+            
+            // Print button
+            out.println("<div class='container-fluid p-3'>");
+            out.println("<button class='btn btn-secondary' onclick='window.print()'>Print Statement</button>");
+            out.println("</div>");
+
+            // Bootstrap table
+            out.println("<div class='container'>");
+            out.println("<h2 class='mt-4'>SBI Statement</h2>");
+            out.println("<h4>User ID: " + Userid + "</h4>");
+            out.println("<table class='table table-striped mt-4'>");
+            out.println("<thead>");
+            out.println("<tr>");
+            out.println("<th>Transaction Date</th>");
+            out.println("<th>Amount</th>");
+            out.println("<th>Transaction Type</th>");
+            out.println("</tr>");
+            out.println("</thead>");
+            out.println("<tbody>");
+
+            while (resultSet.next()) {
+                out.println("<tr>");
+                out.println("<td>" + resultSet.getDate(1) + "</td>");
+                out.println("<td>" + resultSet.getDouble(2) + "</td>");
+                out.println("<td>" + resultSet.getString(3) + "</td>");
+                out.println("</tr>");
+            }
+
+            out.println("</tbody>");
             out.println("</table>");
+            out.println("</div>");
+
+            preparedstatement = connection.prepareStatement("Select balance from Account_Details where User_id=?");
+            preparedstatement.setString(1, httpsession.getAttribute("u_id").toString());
+            ResultSet resultsetamount = preparedstatement.executeQuery();
+
+            if (resultsetamount.next()) {
+                out.println("<h4 class='text-center mt-4'>Total Amount: Rs. " + resultsetamount.getDouble(1) + "</h4>");
+            } else {
+                out.println("Unable to calculate");
+            }
+
+            out.println("<script src='https://code.jquery.com/jquery-3.5.1.slim.min.js'></script>");
+            out.println("<script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js'></script>");
+            out.println("<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'></script>"); // Add Bootstrap JavaScript
+
             out.println("</body>");
             out.println("</html>");
-           
-        
-            }
-            
-                     
-            
-            
         }
-    
-        
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(Statement.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(Statement.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
-}
-    
+    }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        return "Statement Servlet";
+    }
 }
