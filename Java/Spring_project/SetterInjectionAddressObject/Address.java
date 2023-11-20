70d5e34b89f1ba1b4c7e7fb9b708d1aef3448646
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-11-20 23:26:32 +0530

    Spring_project

---
+++ Java/Spring_project/SetterInjectionAddressObject/Address.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Address;
+
+/**
+ *
+ * @author Abhishek
+ */
+public class Address {
+     private String addressline1;
+     private String City;
+     private String State;
+     private String Country;
+
+    public String getAddressline1() {
+        return addressline1;
+    }
+
+    public void setAddressline1(String addressline1) {
+        this.addressline1 = addressline1;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+     
+    
+}
---
+++ Java/Spring_project/SetterInjectionAddressObject/Employee.java
@@ -0,0 +1,47 @@
+package Employee;
+
+import Address.Address;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abhishek
+ */
+public class Employee {
+    private int id;
+    private String name;
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void displayinfo(){
+        System.out.println(id+" "+name);
+        System.out.println(address);
+    }
+}
