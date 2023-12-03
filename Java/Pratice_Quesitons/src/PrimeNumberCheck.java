51f59c68b5abb965a7d719559a4dcb2ec4490658
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-12-03 22:53:12 +0530

    java pratice

--- Java/Pratice_Quesitons/src/PrimeNumberCheck.java
+++ Java/Pratice_Quesitons/src/PrimeNumberCheck.java
@@ -1,24 +1,40 @@
+import java.util.Scanner;
+
 public class PrimeNumberCheck {
 
     public static void main(String[] args) {
-        System.out.println(isPrime(19)); // true
-        System.out.println(isPrime(49)); // false
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println((a)+"even"); // true
+        System.out.println((b)+"odd"); // false
+
+
+        if (a%2==0){
+            System.out.println("number is even");
+        } else if (a%2!=0) {
+            System.out.println("number is odd");
+
+        }
+        else {
+            System.out.println(0);
+        }
     }
 
-    public static boolean isPrime(int n) {
-        if (n == 0 || n == 1) {
-            return false;
-        }
-        if (n == 2) {
-            return true;
-        }
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+//    public static boolean isPrime(int n) {
+//        if (n == 0 || n == 1) {
+//            return false;
+//        }
+//        if (n == 2) {
+//            return true;
+//        }
+//        for (int i = 2; i <= n / 2; i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 
 }
---
+++ Java/Pratice_Quesitons/src/spring_pratice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class spring_pratice {
+    public static void main (String args[]){
+        int a = 12;
+        int b=34;
+        Scanner scanner = new Scanner(System.in);
+        
+
+    }
+}
