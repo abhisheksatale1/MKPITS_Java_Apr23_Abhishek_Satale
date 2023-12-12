07598dc6943d55036f72ea4e98930c1e976ce896
abhisheksatale1 <abhisheksatale4@gmail.com>
2023-12-12 10:28:26 +0530

    Exception Handling in SpringBoot

---
+++ Spring_boot/StudentManagementCrudOperationExceptionhandling/StudentManagementCrudOperation/src/main/java/com/example/StudentManagementCrudOperation/Service/ServiceClass.java
@@ -0,0 +1,16 @@
+package com.example.StudentManagementCrudOperation.Service;
+
+import com.example.StudentManagementCrudOperation.Dao.StudentDao;
+import com.example.StudentManagementCrudOperation.EntityClass.Student;
+import org.springframework.web.bind.annotation.RequestAttribute;
+
+import java.util.List;
+public interface ServiceClass {
+
+String InventoryItembyId(Student thestudent);
+    public List <Student> getalldata();
+
+Student updateRecord(Integer rollno);
+
+void deleteRecord(Integer rollno);
+}
---
+++ Spring_boot/StudentManagementCrudOperationExceptionhandling/StudentManagementCrudOperation/src/main/java/com/example/StudentManagementCrudOperation/StudentManagementCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagementCrudOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementCrudOperationApplication.class, args);
+	}
+
+}
